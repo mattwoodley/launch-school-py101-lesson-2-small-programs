993df6ccf6f6237455b6ed62bb7b2cd29f065d0b
mattwoodley <m17woodley@gmail.com>
2024-06-23 17:29:35 +0100

    added shortened inputs, reorganised functions and constants within file

--- Venv310/Assignments/rock_paper_scissors/rock_paper_scissors.py
+++ Venv310/Assignments/rock_paper_scissors/rock_paper_scissors.py
@@ -13,43 +13,31 @@ import os
 import json
 import random
 
-with open('rock_paper_scissors_messages.json', encoding="utf-8") as file:
-    MESSAGES = json.load(file)
-VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
-WINNING_CONDITIONS = {
-    'rock': ['scissors', 'lizard'],
-    'paper': ['rock', 'spock'],
-    'scissors': ['paper', 'lizard'],
-    'lizard': ['paper', 'spock'],
-    'spock': ['rock', 'scissors']
-}
 
-
-def prompt(display_message):
+# MAIN FUNCTIONS
+def main():
     """
-    Prints a user message with a prefix.
-
-    Args:
-        display_message (str): The message to be printed.
+    Main function that runs the rock, paper, scissors, lizard, spock game.
+    Prompts the user for input, gets the computer's choice,
+    determines the winner, displays the result,
+    and prompts the user to continue or quit.
     """
-    print(f'==> {display_message}')
+    while True:
+        user_choice = get_user_choice()
+        computer_choice = get_computer_choice()
+        prompt(MESSAGES['user_choice'].format(
+            user_choice=user_choice.capitalize()))
+        prompt(MESSAGES['computer_choice'].format(
+            computer_choice=computer_choice.capitalize()))
+        game_result = determine_winner(user_choice, computer_choice)
+        display_result(game_result, user_choice, computer_choice)
 
+        if not play_again():
+            break
 
-def invalid_choice(choice):
-    """
-    Checks if the user's choice is valid
-    (rock, paper, scissors, lizard, or spock).
+        clear_screen()
 
-    Args:
-        choice (str): The user's choice.
-
-    Returns:
-        bool: True if the choice is invalid, otherwise False.
-    """
-    if choice.strip().lower() in VALID_CHOICES:
-        return False
-
-    return True
+    prompt(MESSAGES['thanks_for_playing'])
 
 
 def get_user_choice():
@@ -59,16 +47,18 @@ def get_user_choice():
     Returns:
         str: The user's valid choice.
     """
-    choices_display = ', '.join(VALID_CHOICES)
-    prompt(MESSAGES['choose_rps'].format(choices=choices_display))
-    choice = input()
+    prompt(MESSAGES['choose_rps'].format(choices=DISPLAY_CHOICES))
+    choice = input().strip().lower()
 
     while invalid_choice(choice):
         prompt(MESSAGES['error_invalid'])
-        prompt(MESSAGES['choose_rps']).format(choices=choices_display)
-        choice = input()
+        prompt(MESSAGES['choose_rps'].format(choices=DISPLAY_CHOICES))
+        choice = input().strip().lower()
 
-    return choice.lower()
+    if len(choice) <= 2:
+        return CHOICES_SHORTHAND[choice]
+
+    return choice
 
 
 def get_computer_choice():
@@ -117,14 +107,77 @@ def display_result(result, user_choice, computer_choice):
     """
     match result:
         case 'draw':
-            display_box('result_draw', user_choice=user_choice,
-                                       computer_choice=computer_choice)
+            display_box('result_draw',
+                        user_choice=user_choice.capitalize(),
+                        computer_choice=computer_choice.capitalize())
         case 'win':
-            display_box('user_wins', user_choice=user_choice,
-                                       computer_choice=computer_choice)
+            display_box('user_wins',
+                        user_choice=user_choice.capitalize(),
+                        computer_choice=computer_choice.capitalize())
         case 'loss':
-            display_box('user_loses', user_choice=user_choice,
-                                       computer_choice=computer_choice)
+            display_box('user_loses',
+                        user_choice=user_choice.capitalize(),
+                        computer_choice=computer_choice.capitalize())
+
+
+def play_again():
+    """
+    Prompts the user to determine if they want to play another game.
+
+    Returns:
+        bool: True if the user wants to continue, otherwise False.
+    """
+    prompt(MESSAGES['continue_playing'])
+    continue_playing = input().strip().lower()
+    return continue_playing in {'yes', 'y'}
+
+
+# HELPER FUNCTIONS
+def invalid_choice(choice):
+    """
+    Checks if the user's choice is valid
+    (rock, paper, scissors, lizard, or spock).
+
+    Args:
+        choice (str): The user's choice.
+
+    Returns:
+        bool: True if the choice is invalid, otherwise False.
+    """
+
+    if not isinstance(choice, str):
+        return True
+
+    return choice not in VALID_CHOICES and choice not in CHOICES_SHORTHAND
+
+
+def prompt(display_message):
+    """
+    Prints a user message with a prefix.
+
+    Args:
+        display_message (str): The message to be printed.
+    """
+    print(f'==> {display_message}')
+
+
+def display_choices():
+    """
+    Constructs a formatted string displaying available choices for the game.
+
+    Retrieves each key-value pair from CHOICES_SHORTHAND, formats as
+    "{Value}/{Key}" with capitalized first letters, and joins into a
+    comma-separated string.
+
+    Returns:
+        str: Formatted string displaying choices, e.g. "Rock/R, Paper/P,
+             Scissors/Sc, Lizard/L, Spock/Sp".
+    """
+    choices_display = []
+    for key, value in CHOICES_SHORTHAND.items():
+        choices_display.append(f"{value.capitalize()}/{key.capitalize()}")
+    choices_display = ", ".join(choices_display)
+    return choices_display
 
 
 def display_box(key, user_choice, computer_choice):
@@ -145,17 +198,6 @@ def display_box(key, user_choice, computer_choice):
     print(f'| {message} |')
     print(border)
 
-def play_again():
-    """
-    Prompts the user to determine if they want to play another game.
-
-    Returns:
-        bool: True if the user wants to continue, otherwise False.
-    """
-    prompt(MESSAGES['continue_playing'])
-    continue_playing = input().lower()
-    return continue_playing == 'yes' or continue_playing == 'y'
-
 
 def clear_screen():
     """
@@ -171,30 +213,46 @@ def clear_screen():
         os.system('cls')
 
 
-def main():
-    """
-    Main function that runs the rock, paper, scissors, lizard, spock game.
-    Prompts the user for input, gets the computer's choice,
-    determines the winner, displays the result,
-    and prompts the user to continue or quit.
-    """
-    clear_screen()
-    while True:
-        user_choice = get_user_choice()
-        computer_choice = get_computer_choice()
-        prompt(MESSAGES['user_choice'].format(user_choice=user_choice))
-        prompt(MESSAGES['computer_choice'].format(
-            computer_choice=computer_choice))
-        game_result = determine_winner(user_choice, computer_choice)
-        display_result(game_result, user_choice, computer_choice)
+# CONSTANTS
 
-        if not play_again():
-            break
+# Load messages from JSON file
+with open('rock_paper_scissors_messages.json', encoding="utf-8") as file:
+    MESSAGES = json.load(file)
 
-        clear_screen()
+# Valid choices for the game
+VALID_CHOICES = [
+    'rock',
+    'paper',
+    'scissors',
+    'lizard',
+    'spock'
+]
 
-    prompt(MESSAGES['thanks_for_playing'])
+# Shorthand mappings for user inputs
+CHOICES_SHORTHAND = {
+    'r': 'rock',
+    'p': 'paper',
+    'sc': 'scissors',
+    'l': 'lizard',
+    'sp': 'spock'
+}
 
+# Winning conditions for the game
+WINNING_CONDITIONS = {
+    'rock': ['scissors', 'lizard'],
+    'paper': ['rock', 'spock'],
+    'scissors': ['paper', 'lizard'],
+    'lizard': ['paper', 'spock'],
+    'spock': ['rock', 'scissors']
+}
 
-prompt(MESSAGES['welcome'])
+# Display the title of the game based on choices
+DISPLAY_TITLE = ", ".join([choice.capitalize() for choice in VALID_CHOICES])
+
+# Display choices for prompt messages
+DISPLAY_CHOICES = display_choices()
+
+# Start the game
+clear_screen()
+prompt(MESSAGES['welcome'].format(choices=DISPLAY_TITLE))
 main()
